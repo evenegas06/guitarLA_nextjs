ed34f0641aa145dd9ed2ab846341004069f9321e
Eduardo Venegas <evenegas06@hotmail.com>
2023-07-22 01:43:42 -0600

    Header styles and check active page... #251

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,46 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import styles from "@/css/header.module.css";
 import logo from '../../public/imgs/logo.svg';
 
+
 const Header = () => {
+    /* ----- Hooks ----- */
+    const pathname = usePathname();
+
     return (
         <header className={styles.header}>
             <div className={`container ${styles.navbar}`}>
-                <Image src={logo} alt="Logotipo - GuitarLA" />
+                <Link href="/">
+                    <Image src={logo} width={300} height={40} alt="Logotipo - GuitarLA" />
+                </Link>
 
                 <nav className={styles.nav}>
-                    <Link className={styles.test} href="/">
+                    <Link href="/"
+                        className={pathname === '/' ? styles.active : ''}
+                    >
                         Inicio
                     </Link>
 
-                    <Link href="/nosotros">
+                    <Link href="/nosotros"
+                        className={pathname === '/nosotros' ? styles.active : ''}
+                    >
                         Nosotros
                     </Link>
 
-                    <Link href="/blog">
+                    <Link href="/blog"
+                        className={pathname === '/blog' ? styles.active : ''}
+                    >
                         Blog
                     </Link>
 
-                    <Link href="/tienda">
+                    <Link href="/tienda"
+                        className={pathname === '/tienda' ? styles.active : ''}
+                    >
                         Tienda
                     </Link>
                 </nav>
